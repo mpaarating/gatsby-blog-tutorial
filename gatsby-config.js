46dc3a01a49c08646fb2b1a2e705a4523cde947e
mpaarating <alexmichael.lewis@gmail.com@gmail.com>
2017-12-22 17:16:32 -0500

    Create graphql queries for Contentful data and display it on index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-  },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `5t78d498o5f0`,
+        accessToken: `781855a7603482584dc0cae7a90771a4ff7ae75c1c2b7e690613ba2e914561db`
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,61 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import g from "glamorous";
+import Link from "gatsby-link";
 
-const IndexPage = () => (
+import { rhythm } from "../utils/typography";
+
+const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <g.Div>
+      <g.H1 fontSize={rhythm(1)}>Hi people</g.H1>
+      {data.allContentfulPost.edges.map(({ node }) => (
+        <g.Div>
+          <g.Div textAlign={`center`}>
+            {node.featuredImage ? (
+              <g.Img src={node.featuredImage.file.url} />
+            ) : null}
+          </g.Div>
+
+          <h1>{node.title.title}</h1>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: node.body.childMarkdownRemark.html
+            }}
+          />
+          <hr />
+        </g.Div>
+      ))}
+    </g.Div>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const query = graphql`
+  query IndexQuery {
+    allContentfulPost {
+      edges {
+        node {
+          author {
+            name
+          }
+          createdAt
+          body {
+            childMarkdownRemark {
+              html
+            }
+          }
+          title {
+            title
+          }
+          featuredImage {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
